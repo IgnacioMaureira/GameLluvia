e7b577d58a2dfee623c89ab7781667a605a1c12c
Ignacio Maureira Torres <143913133+IgnacioMaureira@users.noreply.github.com>
2025-11-21 18:16:55 -0300

    Update Tarro.java

--- Tarro.java
+++ Tarro.java
@@ -67,13 +67,6 @@ public class Tarro {
 	   
 	   
 	   public void actualizarMovimiento() { 
-		   // movimiento desde mouse/touch
-		   /*if(Gdx.input.isTouched()) {
-			      Vector3 touchPos = new Vector3();
-			      touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			      camera.unproject(touchPos);
-			      bucket.x = touchPos.x - 64 / 2;
-			}*/
 		   //movimiento desde teclado
 		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
 		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
