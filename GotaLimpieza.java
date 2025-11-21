a5d050bb37e3a0e04cb25d8bdab93a73fec282bd
Ignacio Maureira Torres <143913133+IgnacioMaureira@users.noreply.github.com>
2025-11-21 18:10:54 -0300

    Update GotaLimpieza.java

--- GotaLimpieza.java
+++ GotaLimpieza.java
@@ -1,68 +1,52 @@
 package com.mygame.rain.entities;
 
+import com.mygame.rain.interfaces.MovimientoStrategy;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygame.rain.interfaces.Collectable;
 
 //Power UP
-public class GotaLimpieza extends GameObject implements Collectable{
-	private static final float velocidad_Caida = 150f; // Más lenta (más fácil de atrapar)
-    private static final int puntos_Bonus = 20;
-    
-    private float velocidad;
-    private final int puntos;
+public class GotaLimpieza extends GameObject implements Collectable {
+
+    private static final int PUNTOS_BONUS = 20;
     private boolean recolectada;
-    
-    public GotaLimpieza(float x, float y, Texture texture) {
+
+    public GotaLimpieza(float x, float y, Texture textura, MovimientoStrategy estrategia) {
         super(x, y, 64, 64);
-        setTexture(texture);
-        this.velocidad = velocidad_Caida;
-        this.puntos = puntos_Bonus;
+        setTexture(textura);
         this.recolectada = false;
+
+        setEstrategiaMovimiento(estrategia);
     }
-    
+
     @Override
-    public void update(float deltaTime) {
-        if (isActive() && !recolectada) {
-            setY(getY() - velocidad * Gdx.graphics.getDeltaTime());
-        }
+    protected void mover(float deltaTime) {
+        aplicarEstrategiaMovimiento(deltaTime);
     }
-    
-    // Implementación de Collectable
+
+    @Override
     public int getPoints() {
-        return puntos;
+        return PUNTOS_BONUS;
     }
-    
+
+    @Override
     public void onCollect() {
         if (isCollectable()) {
             recolectada = true;
             setActive(false);
-            System.out.println("¡LIMPIEZA! Todas las gotas malas eliminadas. +" + puntos + " puntos");
+            System.out.println("¡LIMPIEZA! Todas las gotas malas eliminadas. +" + PUNTOS_BONUS + " puntos");
         }
     }
-    
+
+    @Override
     public boolean isCollectable() {
         return isActive() && !recolectada;
     }
-    
-    
-     //Identifica que esta es una gota de limpieza 
+
     public boolean esGotaLimpieza() {
         return true;
     }
-    
-    public float getVelocidad() {
-        return velocidad;
-    }
-    
-    public void setVelocidad(float velocidad) {
-        if (velocidad < 0) {
-            throw new IllegalArgumentException("Velocidad debe ser positiva");
-        }
-        this.velocidad = velocidad;
-    }
-    
+
     @Override
-    public void dispose() {
-    }
+    public void dispose() {}
 }
